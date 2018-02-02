2c2318e9dedb62f5f72fecf170baa30859de96ba
Dariusz Nurzyński <scorpion7812@gmail.com>
2018-02-02 08:42:18 +0100

    Now including css and js is realy repaired

--- src/main/java/pl/ndsm/conf/ResourcesViewConf.java
+++ src/main/java/pl/ndsm/conf/ResourcesViewConf.java
@@ -7,9 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class ResourcesViewConf extends WebMvcConfigurerAdapter {
 
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-            .addResourceLocations("classpath:/resources/");
+            .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        
+        //registry.addResourceHandler("/webjars/**")
+        	//.addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
