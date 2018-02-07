268c79b0a56af83927b8af4d0c88ceb6bad18bbe
Dariusz Nurzyński <scorpion7812@gmail.com>
2018-02-07 11:02:56 +0100

    Defining new globlas variables

--- bin/static/js/globals.js
+++ bin/static/js/globals.js
@@ -12,3 +12,16 @@ var MAIN_ENDPOINT = "/socket";
 var userChanel;
 
 var stompClient;
+
+var USER_CHANEL_WR_CODES = {
+	MOVE_TO_GAME: 0 	
+};
+
+var USER_CHANEL_GR_CODES = {
+	RESULT_OF_OWN_SHOT: 0,
+	RESULT_OF_ENEMY_SHOT: 1,
+	LOSS_OF_OWN_TIME: 2,
+	LOSS_OF_ENEMY_TIME: 3,
+	VICTORY_RESULT: 4,
+	LOSS_MATCH: 5,
+}
---
+++ bin/static/js/ship_definition.js
@@ -0,0 +1,3 @@
+var SHIP_DEFINITION = [{
+	
+}]
